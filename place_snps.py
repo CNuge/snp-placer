2c61980cbf6cd9717c4d0ba0eafc3e7757622ba9
WATTBA <nugentc@uoguelph.ca>
2017-06-01 14:45:19 -0400

    cleanup and output writing

--- place_snps.py
+++ place_snps.py
@@ -1,14 +1,7 @@
-
 import cigarParse
 import pandas as pd
 from pandas import Series, DataFrame
 from itertools import groupby
-
-
-
-
-""" possibly, make this into an argparse style function"""
-import argparse
 
 
 def sam_subset(snp_names, sam_file_df):
@@ -56,110 +49,55 @@ def snp_contig_location(flag, pos, adjusted_bp_location, alignment_length):
 
 
 def snp_placement_dataframe(sam_dataframe):
-	"""applies snp_contig_location across a dataframe """
+	"""apply snp_contig_location across a dataframe """
 	sam_dataframe['contig_location'] = sam_dataframe.apply(
 		lambda x: snp_contig_location(x['Flag'], x['Pos'], x['adjusted_bp_SNP_location'], x['alignment_length']), axis=1)
 	return sam_dataframe
-"""
-	this is done like so:
-	if column 2 indicates a forward alignment (0 or 256):
-		1. get bp of snp using cigar_string.cigar_string_change()
-
-	elif column 2 indicates reverse alignment (16 or 272):
-		1. count bp in alignment using cigar_string.alignment_counter()
-		2. get bp of snp using cigar_string.cigar_string_change()
-		3. use column 4 of sam to get leftmost bp of alignment
-
-		bp_on_contig = POS + (output of cigar_string.alignment_counter()) - (output of cigar_string.cigar_string_change())
-"""
-
-
-snp_data_on_contigs = sam_subset(snp_input_dat['SNP_name'], sam_dat)
-
-all_polymorphism_data = sam_polymorphism_column_merger(snp_data_on_contigs, snp_input_dat)
-
-pre_placement = calculate_new_bp_data(all_polymorphism_data)
-
-placement = snp_placement_dataframe(pre_placement)
-
-# columns of interest ['Qname','Flag','Rname','Pos','MapQ','Cigar']
-
-name = 'SAC14'
-
-
-
-"""
-here we will read in the data from the .sam file
-and also read the data from the SNP sequence files.
-
-
-
-build a single dataframe with all of the SNP files in it
-add new columns to the SNP dataframe
-specifically : bp_on_contig,contig,alignment_type
-
-
-- where bp_on_contig is determined using:
-	- using the functions in cigar_string.py:
-		a. adjusted bp of snp
-		b. length of alignmet
-	+ the alignment start location (column 4 of SAM == POS)
-	+ the reverse flag column (2) (0 or 256 ==  simple addition, 16 or 272 == do math to reverse the bp position with the read)
-	+ adjusted bp of snp
-
-- where contig is based on the .sam Rname column.
-
-- where the alignment type is specified as either:
-	'complete' or 'partial' based on: 
-	- the .sam file's cigar string M+len(sequence) == complete
-
-
-
-
-BE REALLY CAEFUL AND DOUBLE CHECK THE MATH AT EACH POINT SO THAT EVERYTHING GOES TO
-THE RIGHT SPOT... THIS IS ABSLOUTELY CRITICAL!!!!!!!!!
-
-"""
-
-
 
 
 if __name__ == '__main__':
 
-""" reading in files """
-
-""" sam file """
-sam_header = ['Qname','Flag','Rname','Pos','MapQ','Cigar','Rnext','Pnext', 'TLEN', 'SEQ', 'QUAL','tag','type','value']
-
-sam_input_file = './sam_files/all_snps_samfile_one_location_alignments.sam'
-sam_dat = pd.read_table(sam_input_file, sep='\t', names = sam_header, index_col=None)
-
-#take the brackets out of the query section
-sam_dat['Qname'] = [x.split('(')[0] for x in sam_dat['Qname']]
-
-#sam data now usable
-sam_dat.head()
 
 
-""" snp data """
+	#read in sam file
+	sam_header = ['Qname','Flag','Rname','Pos','MapQ','Cigar','Rnext','Pnext', 'TLEN', 'SEQ', 'QUAL','tag','type','value']
 
-snp_input_file1 = './snp_files/Stacks_SNP_info.txt'
-snp_dat1 = pd.read_table(snp_input_file1, sep='\t', index_col=None)
+	sam_input_file = './sam_files/all_snps_samfile_one_location_alignments.sam'
+	sam_dat = pd.read_table(sam_input_file, sep='\t', names = sam_header, index_col=None)
 
-snp_input_file2 = './snp_files/Tassel_PSV_info.txt'
-snp_dat2 = pd.read_table(snp_input_file2, sep='\t', index_col=None)
-
-snp_input_file3 = './snp_files/Tassel_SNP_info.txt'
-snp_dat3 = pd.read_table(snp_input_file3, sep='\t', index_col=None)
-
-frames=[snp_dat1,snp_dat2,snp_dat3]
-snp_input_dat = pd.concat(frames)
-
-#snp data now avaliable
-snp_input_dat.head()
+	#take the brackets out of the query section
+	sam_dat['Qname'] = [x.split('(')[0] for x in sam_dat['Qname']]
 
 
 
+	#read in SNP files
+
+	snp_input_file1 = './snp_files/Stacks_SNP_info.txt'
+	snp_dat1 = pd.read_table(snp_input_file1, sep='\t', index_col=None)
+
+	snp_input_file2 = './snp_files/Tassel_PSV_info.txt'
+	snp_dat2 = pd.read_table(snp_input_file2, sep='\t', index_col=None)
+
+	snp_input_file3 = './snp_files/Tassel_SNP_info.txt'
+	snp_dat3 = pd.read_table(snp_input_file3, sep='\t', index_col=None)
+
+	frames=[snp_dat1,snp_dat2,snp_dat3]
+	snp_input_dat = pd.concat(frames)
+
+	#snp data now avaliable
+	snp_input_dat.head()
+
+	#subset the sam alignments for rows matching the snp input
+	snp_data_on_contigs = sam_subset(snp_input_dat['SNP_name'], sam_dat)
+
+	all_polymorphism_data = sam_polymorphism_column_merger(snp_data_on_contigs, snp_input_dat)
+
+	#calculate new bp data
+	all_polymorphism_data = calculate_new_bp_data(all_polymorphism_data)
+
+	all_polymorphism_data  = snp_placement_dataframe(all_polymorphism_data )
+
+	all_polymorphism_data.to_csv('snps_on_contigs_one_location.csv')
 
 
 
