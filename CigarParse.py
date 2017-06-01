1f00605a04370ed66a3c52b55fa586c52534d95d
WATTBA <nugentc@uoguelph.ca>
2017-06-01 09:56:22 -0400

    CigarParse functions tested

--- CigarParse.py
+++ CigarParse.py
@@ -1,40 +1,3 @@
-
-
-
-
-""" example cigar data """
-
-SAC24863(Q30)	0	Contig1825	947267	30	85M	*	0	0	TGCATTCCGATTCTTTCATAGATTACAATGGACACATATTATGTGTGTAAAATATTTTTTTGAAGGTTGTACTGATTATGATGAG	*	RG:Z:group1	MD:Z:58G26	NM:i:1
-SAC24863	T/G	59	TGCATTCCGATTCTTTCATAGATTACAATGGACACATATTATGTGTGTAAAATATTTTTTTGAAGGTTGTACTGATTATGATGAG
-
-
-SAC32530(Q42)	16	Contig785	222749	42	85M	*	0	0	TCCAAATATTCTAAGTCAGAACCGTCCAGAGTAGTGATGCTGGGCGGGCGGGCAGGTGTTGGTAGGATCGGTTAAAAAGCATGCA	*	RG:Z:group1	MD:Z:85	NM:i:0
-SAC32530	C/T	42	TGCATGCTTTTTAACCGATCCTACCAACACCTGCCCGCCCGCCCAGCATCACTACTCTGGACGGTTCTGACTTAGAATATTTGGA
-
-
-SAC55327(Q30)	0	Contig1199	1693992	30	84M1S	*	0	0	TGCATGGAGCGAGACAGGAAACAGGCAGAACAAACTATGGCCTCTTCTCCCCAAAGCCACTTCATCCATCCAGTCTGTTATGTTG	*	RG:Z:group1	MD:Z:84	NM:i:0
-SAC55327	G/T	85	TGCATGGAGCGAGACAGGAAACAGGCAGAACAAACTATGGCCTCTTCTCCCCAAAGCCACTTCATCCATCCAGTCTGTTATGTTG
-
-SAC200463(Q30)	16	Contig1290	5387428	30	84M1S	*	0	0	TCACACTCTCCGTAACTGGTTGCATCACTGCCTGGAATGGCAATTGCTCGGCCTCCGACCGCAAGGCACTACAGAGGGTAATGCA	*	RG:Z:group1	MD:Z:4G79	NM:i:1
-SAC200463	T/C	81	TGCATTACCCTCTGTAGTGCCTTGCGGTCGGAGGCCGAGCAATTGCCATTCCAGGCAGTGATGCAACCAGTTACGGAGAGTGTGA
-
-
-SAC162239(Q30)	0	Contig967	159258	30	74M11S	*	0	0	TGCATTACATAAAGACTTACAGTATACAGAAATCCAAAAGACCGCAAAACCCTGCTACTTGCAGATAGTATGCAAGATCGGAAGA	*	RG:Z:group1	MD:Z:74	NM:i:0
-SAC162239	A/C	63	TGCATTACATAAAGACTTACAGTATACAGAAATCCAAAAGACCGCAAAACCCTGCTACTTGCAGATAGTATGCAAGATCGGAAGA
-
-
-SAC45959(Q0)	0	Contig0	422443	0	52M1D33M	*	0	0	TGCATATTGTACGATCTAGAATAGAGTACGAGGCAGTTTAATTTGGGCACGAGTTTTCCAAAGTGGAAACAGCGCCTCCATATTG	*	RG:Z:group1	MD:Z:52^T0T23C8	NM:i:3
-SAC45959	T/C	77	TGCATATTGTACGATCTAGAATAGAGTACGAGGCAGTTTAATTTGGGCACGAGTTTTCCAAAGTGGAAACAGCGCCTCCATATTG
-
-
-
-sequence_string='TGCATATTGTACGATCTAGAATAGAGTACGAGGCAGTTTAATTTGGGCACGAGTTTTCCAAAGTGGAAACAGCGCCTCCATATTG'
-bp_of_snp=77
-cigar_string='52M1D33M'
-cigar='52M1D33M'
-
-
-
 from itertools import groupby
 
 
@@ -45,15 +8,11 @@ def cigar_cutter(cigar):
 	return cigar_tuples
 
 def adjust_bp(bp_of_snp, cigar_dat):
-	""" scan the cigar dat, making front trims, insertions, and deletions"""
+	""" scan the cigar data, making front trims, insertions, and deletions"""
 	change_to_bp = 0
 	bp_scan = 0
-	for cigar_bit in cigar_dat:
-		if cigar_bit[1] == 'S':
-			"""find the soft clipping strings, subtract from bp location"""
-			change_to_bp -= cigar_bit[0]
-			bp_scan += cigar_bit[0]
-		elif cigar_bit[1] == 'M':
+	for x, cigar_bit in enumerate(cigar_dat):
+		if cigar_bit[1] == 'M':
 			""" count the matches towards the scan, no change to location"""
 			bp_scan += cigar_bit[0]
 		
@@ -65,10 +24,16 @@ def adjust_bp(bp_of_snp, cigar_dat):
 		elif cigar_bit[1] == 'I':
 			"""add one to location, no change to scan count"""
 			change_to_bp += cigar_bit[0]
+		elif cigar_bit[1] == 'S' and (x != (len(cigar_dat) - 1)):
+			"""find the soft clipping strings, subtract from bp location"""
+			change_to_bp -= cigar_bit[0]
+			bp_scan += cigar_bit[0]
+		elif cigar_bit[1] == 'S':
+			bp_scan += cigar_bit[0]
 
 		""" if the scan has passed the snp, we can return the result,"""
 		"""	as no more changes will happen"""
-		if bp_scan >= cigar_bit[0]:
+		if bp_scan >= bp_of_snp:
 				return (bp_of_snp + change_to_bp)
 
 def fringe_snp_check(bp_of_snp, cigar_dat, sequence_string):
@@ -96,6 +61,7 @@ def cigar_string_change(sequence_string, bp_of_snp, cigar_string):
 		if fringe_snp_check(bp_of_snp, cigar_dat, sequence_string) == True:
 			new_bp = 'snp_outside_aligned_region'
 		return new_bp
+
 
 
 def alignment_length(cigar_cutter_output):
