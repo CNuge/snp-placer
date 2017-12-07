2a21d21e5fee1bfa094467a6bc106407aaa828f4
WATTBA <nugentc@uoguelph.ca>
2017-12-07 06:39:16 -0500

    adding argparse

--- filter_sam_file.py
+++ filter_sam_file.py
@@ -1,4 +1,4 @@
-
+import argparse
 """ this program will take a .sam input file (SAMTOOLs format)
 	and separate the data into 3 categories:
 	1. no alignments (discarded)
@@ -8,34 +8,42 @@
 	For 2. and 3. new .sam files are produced with all 
 	information retained from the alignment section"""
 
+def filter_sam(input_sam_file, alignments_one_location, secondary_alignments_out)
+	with open(input_sam_file) as file:
+		for line in file:
+			if line[0] == "@":
+				""" skip the header section"""
+				continue
+			line_split = line.split('\t')
+			if line_split[1] == '4':
+				""" a 4 in column 2 means the read was not aligned"""
+				continue
+			elif line_split[1] == '256' or line_split[1] == '272':
+				""" a 256 or 272 in column 2 means the read aligned to 2+ locations """
+				add_to = open(secondary_alignments_out,'a')
+				add_to.write(line)
+				add_to.close()
+			elif line_split[1] == '0' or line_split[1] == '16':
+				"""a 0 or 16 in column 2 means the read aligned to one location"""
+				add_to = open(alignments_one_location,'a')
+				add_to.write(line)
+				add_to.close()
+			else:
+				
+				""" there are other flags in this column, that do no appear in the dataset I'm working on"""
+				print('this line not in the categories this program accounts for:\n')
+				print(line)
 
-input_sam_file = './example_data/unfiltered_sam_data.sam'
-
-secondary_alignments_out = './example_data/example_secondary_alignments.sam'
-alignments_one_location = './example_data/samfile_one_location_alignments.sam'
+if __name__ == '__main__':
 
 
-with open(input_sam_file) as file:
-	for line in file:
-		if line[0] == "@":
-			""" skip the header section"""
-			continue
-		line_split = line.split('\t')
-		if line_split[1] == '4':
-			""" a 4 in column 2 means the read was not aligned"""
-			continue
-		elif line_split[1] == '256' or line_split[1] == '272':
-			""" a 256 or 272 in column 2 means the read aligned to 2+ locations """
-			add_to = open(secondary_alignments_out,'a')
-			add_to.write(line)
-			add_to.close()
-		elif line_split[1] == '0' or line_split[1] == '16':
-			"""a 0 or 16 in column 2 means the read aligned to one location"""
-			add_to = open(alignments_one_location,'a')
-			add_to.write(line)
-			add_to.close()
-		else:
-			
-			""" there are other flags in this column, that do no appear in the dataset I'm working on"""
-			print('this line not in the categories this program accounts for:\n')
-			print(line)
+
+	input_sam_file = './example_data/unfiltered_sam_data.sam'
+
+	secondary_alignments_out = './example_data/example_secondary_alignments.sam'
+	alignments_one_location = './example_data/samfile_one_location_alignments.sam'
+
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument()
