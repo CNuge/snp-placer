961123aa0c621ee1e85035d1dbe69dbb4524f4a2
WATTBA <nugentc@uoguelph.ca>
2017-12-07 07:12:05 -0500

    argparse finished, example data update

--- filter_sam_file.py
+++ filter_sam_file.py
@@ -15,6 +15,9 @@ import argparse
 	These names can be changed using the input flags
 	-p for primary (one location) outputs and 
 	-s for secondary (multiple location) outputs
+
+	run from command line using:
+	python filter_sam_file.py input_sam_name.sam
 	"""
 
 def filter_sam(input_sam_file, alignments_one_location, secondary_alignments_out):
@@ -47,14 +50,22 @@ def filter_sam(input_sam_file, alignments_one_location, secondary_alignments_out
 
 if __name__ == '__main__':
 
-
-
-	input_sam_file = './example_data/unfiltered_sam_data.sam'
-
-	secondary_alignments_out = './example_data/secondary_alignments.sam'
-	alignments_one_location = './example_data/samfile_one_location_alignments.sam'
-
+	#move these to a tests.py file
+	#input_sam_file = './example_data/unfiltered_sam_data.sam'
+	#secondary_alignments_out = './example_data/secondary_alignments.sam'
+	#alignments_one_location = './example_data/samfile_one_location_alignments.sam'
 
 	parser = argparse.ArgumentParser()
+	parser.add_argument('input', type = str, 
+		help = 'The filename of the .sam file you wish to filter.')
+	parser.add_argument('-p', '--primary', type = str, default = 'samfile_one_location_alignments.sam',
+		help = 'Optional: a name for the output .sam file containing the alignments to one location.\n\
+		Default is: samfile_one_location_alignments.sam')
+	parser.add_argument('-s', '--secondary', type = str, default = 'samfile_two_location_alignments.sam',
+		help = 'Optional: a name for the output .sam file containing the alignments to two or more locations.\n\
+		Default is: samfile_multiple_location_alignments.sam ')
+	args = parser.parse_args(['./example_data/unfiltered_sam_data.sam'])
 
-	parser.add_argument()
+
+	filter_sam(args.input, args.primary, args.secondary)
+
